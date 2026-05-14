47d88d9c76ed2a7bcfbf8222cf462492b383a3f1
dani <daniansyahchusyaidin@gmail.com>
2026-05-15 05:40:51 +0700

    nextjs migration and optimization

--- src/constants/baseUrl.ts
+++ src/constants/baseUrl.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://wedding-dani-marini.web.app';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? 'https://wedding-dm.vercel.app';
